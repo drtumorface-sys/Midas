1ccef0951b05773c78f49fd345e3e8559ec93b35
drtumorface-sys <drtumorface@gmail.com>
2026-03-25 04:51:34 +1100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = '72-phase-v1.0.72';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon-180.png',
+    './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS_TO_CACHE))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.filter(k => k !== CACHE_NAME)
+                    .map(k => caches.delete(k))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cached => cached || fetch(event.request))
+            .catch(() => caches.match('./index.html'))
+    );
+});
